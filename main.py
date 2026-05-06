b806ea82f3d8be00cc66432da976481196deef89
shashlik <shashlichok17@gmail.com>
2026-05-06 20:23:41 +0300

    added-coins

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
 
 streak = 0
+coins = 0
 
 while True:
-    number = random.randint(1, 10)
-    inp = input(f"Choose a number from 1 to 10!" + (f" streak: {streak}\n" if streak > 0 else "\n"))
-    if inp == str(number):
+    num = random.randint(1, 10)
+    inp = input(f"Choose a number from 1 to 10! Coins: {coins}" + (f", streak: {streak}\n" if streak > 0 else "\n"))
+    if inp == str(num):
         print("Correct!")
         streak += 1
+        coins += (10 * (1 + (streak / 10)))
     else:
         print("Wrong!")
         streak = 0
