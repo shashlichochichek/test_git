dd717266fb899bfdb18b534c3f64d515edf2fa50
shashlik <shashlichok17@gmail.com>
2026-05-06 18:55:28 +0300

    first-code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-test = "hi"
+import random
+
+while True:
+    number = random.randint(1, 10)
+    inp = input("Choose a number from 1 to 10")
+    if inp == str(number):
+        print("Correct!")
+    else:
+        print("Wrong!")
