b64026b96f32c3d74ae419a95f1d3e03e90ce339
shashlik <shashlichok17@gmail.com>
2026-05-06 19:44:37 +0300

    added-steak

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 
+streak = 0
+
 while True:
     number = random.randint(1, 10)
-    inp = input("Choose a number from 1 to 10")
+    inp = input(f"Choose a number from 1 to 10!" + (f" streak: {streak}\n" if streak > 0 else "\n"))
     if inp == str(number):
         print("Correct!")
+        streak += 1
     else:
         print("Wrong!")
+        streak = 0
