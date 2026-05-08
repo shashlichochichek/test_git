d89c256aabf4fc693c5405dfd9a8ed0dbd8acc80
shashlik <shashlichok17@gmail.com>
2026-05-08 18:59:19 +0300

    fully-updated-code-and-added-normal-tip

--- main.py
+++ main.py
@@ -2,40 +2,63 @@ import random
 
 streak = 0
 coins = 0
+best_streak = 0
+round_num = 1
 
-def weak_tip_fun(num):
+
+def weak_tip(num):
     global coins
-    rand = random.randint(1, 10)
-    if rand == num:
-        weak_tip_fun(num)
-    else:
-        if (coins - items["weak_tip"][0]) < 0:
-            print("Not enough coins!")
-            return
 
-        print(f"Number is not {rand}!")
-        coins -= items["weak_tip"][0]
+    if coins < items["weak_tip"][0]:
+        print("Not enough coins!")
+        return
+
+    candidates = [x for x in range(1, 11) if x != num]
+    coins -= items["weak_tip"][0]
+    print(f"The answer is NOT {random.choice(candidates)}")
+
+
+def normal_tip(num):
+    global coins
+
+    if coins < items["normal_tip"][0]:
+        print("Not enough coins!")
+        return
+
+    candidates = [x for x in range(1, 11) if x != num]
+    coins -= items["normal_tip"][0]
+    print(f"The answer is NOT any of: {random.sample(candidates, 5)}")
+
 
 items = {
-    "weak_tip" : [5, weak_tip_fun]
+    "weak_tip":   [5,  weak_tip],
+    "normal_tip": [25, normal_tip],
 }
 
-def main(num):
-    global streak
-    global coins
-    inp = input(f"Choose a number from 1 to 10! Coins: {coins}" + (f", streak: {streak}\n" if streak > 0 else "\n"))
-
-    if inp == str(num):
-        print("Correct!")
-        streak += 1
-        coins += (10 * (1 + (streak / 10)))
-    elif inp in items:
-        items[inp][1](num)
-        main(num)
-    elif inp != str(num):
-        print("Wrong!")
-        streak = 0
+num = random.randint(1, 10)
 
 while True:
+    shop_hint = " | " + ", ".join(f"{k} ({v[0]}c)" for k, v in items.items())
+    inp = input(
+        f"\nRound {round_num} | Guess 1-10 | Coins: {coins} | "
+        f"Streak: {streak} | Best: {best_streak}"
+        f"{shop_hint}\n> "
+    ).strip()
+
+    if inp in items:
+        items[inp][1](num)
+        continue
+
+    if inp == str(num):
+        streak += 1
+        if streak > best_streak:
+            best_streak = streak
+        reward = int(10 * (1 + streak / 10))
+        coins += reward
+        print(f"Correct! +{reward} coins (streak: {streak})")
+    else:
+        streak = 0
+        print(f"Wrong! The number was {num}")
+
     num = random.randint(1, 10)
-    main(num)
+    round_num += 1
